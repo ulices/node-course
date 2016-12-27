1f8186600a9f2708cc06652a73713cad36ef09df
ulices <ulices.barajas@magmalabs.io>
2016-12-27 16:05:23 -0600

    Web Server: modify index and use PORT env variable

--- web-server/server.js
+++ web-server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 var middleware = require('./middleware.js');
 
 //app.get('/', function (req, res) {
