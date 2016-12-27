538d45b0f97e883e298638e5b060fbeb4d8f48b4
ulices <ulices.barajas@magmalabs.io>
2016-12-27 12:49:31 -0600

    Web Server: Refactor middleware, move logict oustiside the server

--- web-server/server.js
+++ web-server/server.js
@@ -1,23 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleware = require('./middleware.js');
 
 //app.get('/', function (req, res) {
   //res.send('Hello Express!');
 //});
 
-var middleware = {
-  requireAuthentication: function (req, res, next) {
-    console.log('Private route hit!');
-    next();
-  },
-  logger: function (req, res, next) {
-    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-    next();
-  }
-};
-
-//app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
