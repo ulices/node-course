975e30a05db5c8bdaac2460d01bd66f63a18af0c
ulices <ulices.barajas@magmalabs.io>
2016-12-27 12:06:25 -0600

    Web Server: Use middleware to add some logs

--- web-server/server.js
+++ web-server/server.js
@@ -6,7 +6,21 @@ var PORT = 3000;
   //res.send('Hello Express!');
 //});
 
-app.get('/about', function (req, res) {
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+    console.log('Private route hit!');
+    next();
+  },
+  logger: function (req, res, next) {
+    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
   res.send('About Us');
 });
 
